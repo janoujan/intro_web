9deadf752b88060d71c1cef2096f48b4a764d431
janoujan <janou@janou.pro>
2025-06-30 14:49:41 +0200

    fix connexion to infinity in config_infinity.php

--- config_infinity.php
+++ config_infinity.php
@@ -1,13 +1,13 @@
 <?php
  // Paramètres de connexion à la base de données
  // Adaptez avec vos identifiants    
-$host = 'sql303.infinityfree.com';           
+$host = 'sql111.infinityfree.com';           
 // Exemple : sql303.infinityfree.com 
-$user = 'if0_XXXXXXX';              
-$password = 'XXXXXXXX';     
+$user = 'if0_39357160';              
+$password = '180qT1g0fGg3sh';     
 // Votre Identifiant InfinityFree
  // Mot de passe MySQL
- $dbname = 'if0_XXXXXXXX_intro_web';   
+ $dbname = 'if0_39357160_intro_web';   
 // Nom exact de la base
  // Connexion
  $conn = mysqli_connect($host, $user, $password, $dbname);
